071cc0ca9f8fcd27d3f242f7e5ff211283ddca3f
AnFuns <76437286+AnFuns@users.noreply.github.com>
2022-03-17 17:48:16 +0800

    Add files via upload

---
+++ baidu_push.js
@@ -0,0 +1,16 @@
+<!-- 百度自动推送开始 -->
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<!-- 百度自动推送结束 -->
